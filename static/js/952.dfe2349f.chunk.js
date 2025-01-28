49263294415d75811b29505ce0d028c6d87cfcf1
Nicola <nicola@MacBook-Air-di-Valentina.local>
2025-01-28 23:50:38 +0100

    Updates

---
+++ static/js/952.dfe2349f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktraining_app=self.webpackChunktraining_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.dfe2349f.chunk.js.map
